9193b7ccfffc40c50f670fb3b76d2bd0ad2a5a31
Keegan <keegan23.tan@gmail.com>
2024-07-04 17:08:04 +0800

    Update mainh.js

--- mainh.js
+++ mainh.js
@@ -1,7 +1,7 @@
 let playerinfo = [];
-playerinfo[0] = [Test, TestUser1.0, testuser@test.com, 100, 5, 5, 25, f];
-playerinfo[1] = [Keegan, dragonfyre23, keegan23.tan@gmail.com, 100, 5, 5, 25, e];
+playerinfo[0] = [Test, TestUser1, testuser, 100, 5, 5, 25, f];
+playerinfo[1] = [Keegan, dragonfyre23, keegan23.tan, 100, 5, 5, 25, e];
 localStorage.setItem('playerinfo', JSON.stringify(playerinfo));
-let playerinfo = JSON.parse(playerinfo);
-document.write(playerinfo);
-console.log(playerinfo)
+let playerinfo2 = JSON.parse(playerinfo);
+document.write(playerinfo2);
+console.log(playerinfo2);
