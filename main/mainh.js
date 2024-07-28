16b44e66ac7f31d04e353ec005018b4a3eae599f
Keegan <keegan23.tan@gmail.com>
2024-07-28 18:27:21 +0800

    Update mainh.js

--- main/mainh.js
+++ main/mainh.js
@@ -548,7 +548,6 @@ let p25 = `{
         }
 	    }`;
 localStorage.setItem('p25', p25);
-
 function SHA512(str) {
 	/*
 	* Secure Hash Algorithm (SHA512)
@@ -558,12 +557,10 @@ function SHA512(str) {
 		this.highOrder = msint_32;
 		this.lowOrder = lsint_32;
 	}
-
 	var H = [new int64(0x6a09e667, 0xf3bcc908), new int64(0xbb67ae85, 0x84caa73b),
 	new int64(0x3c6ef372, 0xfe94f82b), new int64(0xa54ff53a, 0x5f1d36f1),
 	new int64(0x510e527f, 0xade682d1), new int64(0x9b05688c, 0x2b3e6c1f),
 	new int64(0x1f83d9ab, 0xfb41bd6b), new int64(0x5be0cd19, 0x137e2179)];
-
 	var K = [new int64(0x428a2f98, 0xd728ae22), new int64(0x71374491, 0x23ef65cd),
 	new int64(0xb5c0fbcf, 0xec4d3b2f), new int64(0xe9b5dba5, 0x8189dbbc),
 	new int64(0x3956c25b, 0xf348b538), new int64(0x59f111f1, 0xb605d019),
@@ -604,56 +601,43 @@ function SHA512(str) {
 	new int64(0x3c9ebe0a, 0x15c9bebc), new int64(0x431d67c4, 0x9c100d4c),
 	new int64(0x4cc5d4be, 0xcb3e42b6), new int64(0x597f299c, 0xfc657e2a),
 	new int64(0x5fcb6fab, 0x3ad6faec), new int64(0x6c44198c, 0x4a475817)];
-
 	var W = new Array(64);
 	var a, b, c, d, e, f, g, h, i, j;
 	var T1, T2;
 	var charsize = 8;
-
 	function utf8_encode(str) {
 		return unescape(encodeURIComponent(str));
 	}
-
 	function str2binb(str) {
 		var bin = [];
 		var mask = (1 << charsize) - 1;
 		var len = str.length * charsize;
-
 		for (var i = 0; i < len; i += charsize) {
 			bin[i >> 5] |= (str.charCodeAt(i / charsize) & mask) << (32 - charsize - (i % 32));
 		}
-
 		return bin;
 	}
-
 	function binb2hex(binarray) {
 		var hex_tab = '0123456789abcdef';
 		var str = '';
 		var length = binarray.length * 4;
 		var srcByte;
-
 		for (var i = 0; i < length; i += 1) {
 			srcByte = binarray[i >> 2] >> ((3 - (i % 4)) * 8);
 			str += hex_tab.charAt((srcByte >> 4) & 0xF) + hex_tab.charAt(srcByte & 0xF);
 		}
-
 		return str;
 	}
-
 	function safe_add_2(x, y) {
 		var lsw, msw, lowOrder, highOrder;
-
 		lsw = (x.lowOrder & 0xFFFF) + (y.lowOrder & 0xFFFF);
 		msw = (x.lowOrder >>> 16) + (y.lowOrder >>> 16) + (lsw >>> 16);
 		lowOrder = ((msw & 0xFFFF) << 16) | (lsw & 0xFFFF);
-
 		lsw = (x.highOrder & 0xFFFF) + (y.highOrder & 0xFFFF) + (msw >>> 16);
 		msw = (x.highOrder >>> 16) + (y.highOrder >>> 16) + (lsw >>> 16);
 		highOrder = ((msw & 0xFFFF) << 16) | (lsw & 0xFFFF);
-
 		return new int64(highOrder, lowOrder);
 	}
-
 	function safe_add_4(a, b, c, d) {
 		var lsw, msw, lowOrder, highOrder;
 
@@ -667,7 +651,6 @@ function SHA512(str) {
 
 		return new int64(highOrder, lowOrder);
 	}
-
 	function safe_add_5(a, b, c, d, e) {
 		var lsw, msw, lowOrder, highOrder;
 
@@ -681,21 +664,18 @@ function SHA512(str) {
 
 		return new int64(highOrder, lowOrder);
 	}
-
 	function maj(x, y, z) {
 		return new int64(
 			(x.highOrder & y.highOrder) ^ (x.highOrder & z.highOrder) ^ (y.highOrder & z.highOrder),
 			(x.lowOrder & y.lowOrder) ^ (x.lowOrder & z.lowOrder) ^ (y.lowOrder & z.lowOrder)
 		);
 	}
-
 	function ch(x, y, z) {
 		return new int64(
 			(x.highOrder & y.highOrder) ^ (~x.highOrder & z.highOrder),
 			(x.lowOrder & y.lowOrder) ^ (~x.lowOrder & z.lowOrder)
 		);
 	}
-
 	function rotr(x, n) {
 		if (n <= 32) {
 			return new int64(
@@ -709,7 +689,6 @@ function SHA512(str) {
 			);
 		}
 	}
-
 	function sigma0(x) {
 		var rotr28 = rotr(x, 28);
 		var rotr34 = rotr(x, 34);
@@ -720,7 +699,6 @@ function SHA512(str) {
 			rotr28.lowOrder ^ rotr34.lowOrder ^ rotr39.lowOrder
 		);
 	}
-
 	function sigma1(x) {
 		var rotr14 = rotr(x, 14);
 		var rotr18 = rotr(x, 18);
@@ -731,7 +709,6 @@ function SHA512(str) {
 			rotr14.lowOrder ^ rotr18.lowOrder ^ rotr41.lowOrder
 		);
 	}
-
 	function gamma0(x) {
 		var rotr1 = rotr(x, 1), rotr8 = rotr(x, 8), shr7 = shr(x, 7);
 
@@ -740,7 +717,6 @@ function SHA512(str) {
 			rotr1.lowOrder ^ rotr8.lowOrder ^ shr7.lowOrder
 		);
 	}
-
 	function gamma1(x) {
 		var rotr19 = rotr(x, 19);
 		var rotr61 = rotr(x, 61);
@@ -751,7 +727,6 @@ function SHA512(str) {
 			rotr19.lowOrder ^ rotr61.lowOrder ^ shr6.lowOrder
 		);
 	}
-
 	function shr(x, n) {
 		if (n <= 32) {
 			return new int64(
@@ -765,14 +740,11 @@ function SHA512(str) {
 			);
 		}
 	}
-
 	str = utf8_encode(str);
 	strlen = str.length * charsize;
 	str = str2binb(str);
-
 	str[strlen >> 5] |= 0x80 << (24 - strlen % 32);
 	str[(((strlen + 128) >> 10) << 5) + 31] = strlen;
-
 	for (var i = 0; i < str.length; i += 32) {
 		a = H[0];
 		b = H[1];
@@ -782,14 +754,12 @@ function SHA512(str) {
 		f = H[5];
 		g = H[6];
 		h = H[7];
-
 		for (var j = 0; j < 80; j++) {
 			if (j < 16) {
 				W[j] = new int64(str[j * 2 + i], str[j * 2 + i + 1]);
 			} else {
 				W[j] = safe_add_4(gamma1(W[j - 2]), W[j - 7], gamma0(W[j - 15]), W[j - 16]);
 			}
-
 			T1 = safe_add_5(h, sigma1(e), ch(e, f, g), K[j], W[j]);
 			T2 = safe_add_2(sigma0(a), maj(a, b, c));
 			h = g;
@@ -801,7 +771,6 @@ function SHA512(str) {
 			b = a;
 			a = safe_add_2(T1, T2);
 		}
-
 		H[0] = safe_add_2(a, H[0]);
 		H[1] = safe_add_2(b, H[1]);
 		H[2] = safe_add_2(c, H[2]);
@@ -811,7 +780,6 @@ function SHA512(str) {
 		H[6] = safe_add_2(g, H[6]);
 		H[7] = safe_add_2(h, H[7]);
 	}
-
 	var binarray = [];
 	for (var i = 0; i < H.length; i++) {
 		binarray.push(H[i].highOrder);
@@ -819,16 +787,12 @@ function SHA512(str) {
 	}
 	return binb2hex(binarray);
 };
-
 let players = [p0,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23,p24,p25];
-
 let parsed = [];
 for (i of players) {
 	parsed.append(JSON.parse(i));
 }
-
 let playerList = JSON.stringify(parsed);
-
 // localStorage.clear();
 localStorage.setItem("players", playerList);
 
@@ -856,4 +820,8 @@ function change() {
 function aboutus() {
 	document.location = "https://smilynarwhal77.github.io/others/developers.html"
 }
-/* define signout() */
+function signout() {
+	change();
+	console.log("changed");
+	sessionStorage.clear()
+}
